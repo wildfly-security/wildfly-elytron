6f8d5badc12253f2c2da91c7028aca60adb16847
Darran Lofthouse <darran.lofthouse@jboss.com>
2011-09-07 19:25:49 +0100

    [SASL-4] Add the base class for the SASL mechansism tests.

---
+++ src/test/java/org/jboss/sasl/test/ClientCallbackHandler.java
@@ -0,0 +1,57 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2011, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jboss.sasl.test;
+
+import javax.security.auth.callback.Callback;
+import javax.security.auth.callback.CallbackHandler;
+import javax.security.auth.callback.NameCallback;
+import javax.security.auth.callback.PasswordCallback;
+import javax.security.auth.callback.UnsupportedCallbackException;
+import java.io.IOException;
+
+/**
+ * A simple CallbackHandler for testing the client side of the calls.
+ *
+ * @author <a href="mailto:darran.lofthouse@jboss.com">Darran Lofthouse</a>
+ */
+class ClientCallbackHandler implements CallbackHandler {
+
+    private final String username;
+    private final char[] password;
+
+    ClientCallbackHandler(final String username, final char[] password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public void handle(Callback[] callbacks) throws IOException, UnsupportedCallbackException {
+        for (Callback current : callbacks) {
+            if (current instanceof NameCallback) {
+                NameCallback ncb = (NameCallback) current;
+                ncb.setName(username);
+            } else if (current instanceof PasswordCallback) {
+                PasswordCallback pcb = (PasswordCallback) current;
+                pcb.setPassword(password);
+            }
+        }
+    }
+}
