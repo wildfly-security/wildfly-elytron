270c788d878a42b78903fc0e19c1b6ef4279306f
David M. Lloyd <david.lloyd@redhat.com>
2014-10-06 21:05:14 -0500

    Improve SecurityDomainPermission using security manager facilities

--- src/main/java/org/wildfly/security/auth/permission/SecurityDomainPermission.java
+++ src/main/java/org/wildfly/security/auth/permission/SecurityDomainPermission.java
@@ -18,48 +18,78 @@
 
 package org.wildfly.security.auth.permission;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.ObjectStreamField;
 import java.security.Permission;
+
+import org.wildfly.security.permission.PermissionActions;
 
 public final class SecurityDomainPermission extends Permission {
 
     private static final long serialVersionUID = 8533735187740371169L;
 
-    private final int actions;
+    private static final ObjectStreamField[] serialPersistentFields = new ObjectStreamField[] {
+        new ObjectStreamField("actions", String.class),
+    };
 
-    public SecurityDomainPermission(final String name, final Action... actions) {
+    private transient String actionString;
+    private transient int actions;
+
+    public SecurityDomainPermission(final String name) {
         super(name);
-        int i = 0;
-        for (Action action : actions) {
-            i |= action.i;
-        }
-        this.actions = i;
+        actions = 0;
+    }
+
+    public SecurityDomainPermission(final String name, final String actions) {
+        super(name);
+        this.actions = PermissionActions.parseActionStringToInt(Action.class, actions);
     }
 
     public boolean implies(final Permission permission) {
-        // xxx blah blah
-        return false;
+        return permission instanceof SecurityDomainPermission && implies((SecurityDomainPermission) permission);
+    }
+
+    public boolean implies(final SecurityDomainPermission permission) {
+        return (actions & permission.actions) == permission.actions && getName().equals(permission.getName());
     }
 
     public boolean equals(final Object obj) {
-        return false;
+        return obj instanceof SecurityDomainPermission && equals((SecurityDomainPermission) obj);
+    }
+
+    public boolean equals(final SecurityDomainPermission permission) {
+        return actions == permission.actions && getName().equals(permission.getName());
     }
 
     public int hashCode() {
-        return 0;
+        return getName().hashCode() * 4 + actions;
     }
 
     public String getActions() {
-        return null;
+        String actionString = this.actionString;
+        if (actionString == null) {
+            actionString = this.actionString = PermissionActions.getCanonicalActionString(Action.class, actions);
+        }
+        return actionString;
     }
 
     enum Action {
-        CREATE(1),
-        ACCESS(2),
+        // Do not re-order, ever
+        create,
+        access,
         ;
+    }
 
-        private final int i;
-        Action(final int i) {
-            this.i = i;
-        }
+    private void readObject(ObjectInputStream ois) throws IOException, ClassNotFoundException {
+        final ObjectInputStream.GetField getField = ois.readFields();
+        this.actions = PermissionActions.parseActionStringToInt(Action.class, (String) getField.get("actions", ""));
+    }
+
+    private void writeObject(ObjectOutputStream oos) throws IOException {
+        final ObjectOutputStream.PutField putField = oos.putFields();
+        putField.put("actions", getActions());
+        oos.writeFields();
     }
 }
