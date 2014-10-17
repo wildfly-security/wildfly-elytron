d58f2a91ac2481049980b1dbc4a277d72d6683ff
David M. Lloyd <david.lloyd@redhat.com>
2014-10-16 19:23:34 -0500

    Peer principal callback

---
+++ src/main/java/org/wildfly/security/auth/callback/PeerPrincipalCallback.java
@@ -0,0 +1,54 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2014 Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.wildfly.security.auth.callback;
+
+import java.security.Principal;
+
+/**
+ * An optional callback to inform the callback handler of the peer's principal identity.
+ *
+ * @author <a href="mailto:david.lloyd@redhat.com">David M. Lloyd</a>
+ */
+public final class PeerPrincipalCallback extends AbstractExtendedCallback {
+
+    private static final long serialVersionUID = -2876104318406026491L;
+
+    private final Principal principal;
+
+    /**
+     * Construct a new instance.
+     *
+     * @param principal the peer principal
+     */
+    public PeerPrincipalCallback(final Principal principal) {
+        if (principal == null) {
+            throw new IllegalArgumentException("principal is null");
+        }
+        this.principal = principal;
+    }
+
+    /**
+     * Get the peer principal.
+     *
+     * @return the peer principal
+     */
+    public Principal getPrincipal() {
+        return principal;
+    }
+}
