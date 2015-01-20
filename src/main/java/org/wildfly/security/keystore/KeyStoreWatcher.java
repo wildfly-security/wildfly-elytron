869fed5fc0830496c89f382cfe199b3dfa2c53f7
Darran Lofthouse <darran.lofthouse@jboss.com>
2015-01-20 13:17:10 +0000

    [ELY-118] Simplification so both updates to the collections and use of the collections at notification time are all synchronized.
    A more complex solution can be considered if there is a demonstrated performance bottle neck in the future, at the moment it is just speculation.

--- src/main/java/org/wildfly/security/keystore/KeyStoreWatcher.java
+++ src/main/java/org/wildfly/security/keystore/KeyStoreWatcher.java
@@ -29,7 +29,6 @@ import java.nio.file.WatchEvent;
 import java.nio.file.WatchKey;
 import java.nio.file.WatchService;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -59,10 +58,8 @@ class KeyStoreWatcher {
      *  So we have a {@link Map} where the key is a {@link Path} to the directory being monitored, the value is a further {@link Map}.
      *  On the second {@link Map} the key is the name of the keystore file within the the directory being monitored and the value is a {@link List}
      *  The {@link List} referenced by the {@link Map} is a list of all keystores that should be reloaded if a modification to the file is detected.
-     *
-     *  Note: This is defined as 'volatile' as on writes the whole {@link Map} will be replaced.
      */
-    private volatile Map<Path, Map<String, List<Store>>> watchedPaths = Collections.emptyMap();
+    private final Map<Path, Map<String, List<Store>>> watchedPaths = new HashMap<Path, Map<String,List<Store>>>();
     private final Map<Path, WatchKey> registrations = new HashMap<Path, WatchKey>();
 
     private volatile WatchService watchService;
@@ -77,7 +74,7 @@ class KeyStoreWatcher {
         return theWatcher;
     }
 
-    synchronized void register(File watchFile, Store keyStore) throws IOException {
+    void register(File watchFile, Store keyStore) throws IOException {
         File canonical = watchFile.getCanonicalFile();
 
         File parentDir = canonical.getParentFile();
@@ -85,55 +82,40 @@ class KeyStoreWatcher {
 
         Path dirPath = parentDir.toPath();
 
-        boolean watchRequired = false;
-        Map<String, List<Store>> pathRegistration = watchedPaths.get(dirPath);
-        List<Store> pathStores = null;
-        if (pathRegistration == null) {
-            // There is no existing watch of this directory so we will need to start it but only do that after the Map has been updated.
-            watchRequired = true;
-            pathRegistration = new HashMap<String, List<Store>>();
-            pathStores = new ArrayList<Store>();
-        } else {
-            // We are adding to an existing Map so create a copy.
-            pathRegistration = new HashMap<String, List<Store>>(pathRegistration);
-            List<Store> tmpStore = pathRegistration.get(fileName);
-            if (tmpStore != null) {
-                // Copy the store so we can add to it without affecting any iterator.
-                pathStores = new ArrayList<Store>(tmpStore);
-            } else {
+        synchronized (watchedPaths) {
+            Map<String, List<Store>> pathRegistration = watchedPaths.get(dirPath);
+            List<Store> pathStores = null;
+            if (pathRegistration == null) {
+                if (watchService == null) {
+                    // Is this the very first Path to be watched, if so we are going to need a WatchService.
+                    watchService = fileSystem.newWatchService();
+                    Thread pollThread = new Thread(new EventTaker(), "KeyStoreWatcher Daemon");
+                    pollThread.setDaemon(true);
+                    pollThread.start();
+                }
+                // We use 'create' in addition to 'modify' as updates could be in the form of replacing a file.
+                WatchKey key = dirPath.register(watchService, StandardWatchEventKinds.ENTRY_CREATE, StandardWatchEventKinds.ENTRY_MODIFY);
+                // We also need to maintain a mapping with the WatchKey so we have something we can use to Cancel the
+                // registration latser.
+                registrations.put(dirPath, key);
+
+                pathRegistration = new HashMap<String, List<Store>>();
+                watchedPaths.put(dirPath, pathRegistration);
                 pathStores = new ArrayList<Store>();
+                pathRegistration.put(fileName, pathStores);
+            } else {
+                pathStores = pathRegistration.get(fileName);
+                if (pathStores == null) {
+                    pathStores = new ArrayList<Store>();
+                    pathRegistration.put(fileName, pathStores);
+                }
             }
-        }
 
-        /*
-         * Modifications should not happen to any of these collections but to detect errors
-         * in the future make all unmodifiable.
-         */
-        pathStores.add(keyStore);
-        // The pathStores is the one we created one way or another above.
-        pathRegistration.put(fileName, Collections.unmodifiableList(pathStores));
-        // We are making modifications so copy watchedPaths
-        Map<Path, Map<String, List<Store>>> newWatchedPaths = new HashMap<Path, Map<String, List<Store>>>(watchedPaths);
-        // pathRegistration was also created one way or another above.
-        newWatchedPaths.put(dirPath, Collections.unmodifiableMap(pathRegistration));
-        watchedPaths = Collections.unmodifiableMap(newWatchedPaths);
-
-        if (watchRequired) {
-            if (watchService == null) {
-                // Is this the very first Path to be watched, if so we are going to need a WatchService.
-                watchService = fileSystem.newWatchService();
-                Thread pollThread = new Thread(new EventTaker(), "KeyStoreWatcher Daemon");
-                pollThread.setDaemon(true);
-                pollThread.start();
-            }
-            // We use 'create' in addition to 'modify' as updates could be in the form of replacing a file.
-            WatchKey key = dirPath.register(watchService,  StandardWatchEventKinds.ENTRY_CREATE, StandardWatchEventKinds.ENTRY_MODIFY);
-            // We also need to maintain a mapping with the WatchKey so we have something we can use to Cancel the registration latser.
-            registrations.put(dirPath, key);
+            pathStores.add(keyStore);
         }
     }
 
-    synchronized void deRegister(File watchFile, Store keyStore) throws IOException {
+    void deRegister(File watchFile, Store keyStore) throws IOException {
         File canonical = watchFile.getCanonicalFile();
 
         File parentDir = canonical.getParentFile();
@@ -141,54 +123,41 @@ class KeyStoreWatcher {
 
         Path dirPath = parentDir.toPath();
 
-        Map<String, List<Store>> pathRegistration = watchedPaths.get(dirPath);
-        List<Store> pathStores = null;
-        if (pathRegistration != null) {
-            // The act of removal means we know we will be replacing the list of keystores requiring notification, it
-            // may also result in this file being no-longer monitored - either way we need a copy to work on.
-            pathRegistration = new HashMap<String, List<Store>>(pathRegistration);
-            List<Store> tmpStores = pathRegistration.get(fileName);
-            if (tmpStores != null) {
-                // Remove all references to this store from a copy of the list of stores to be notified about this file.
-                pathStores = new ArrayList<Store>(tmpStores);
-                Iterator<Store> storeIterator = pathStores.iterator();
-                while (storeIterator.hasNext()) {
-                    Store current = storeIterator.next();
-                    if (keyStore == current) {
-                        storeIterator.remove();
+        synchronized (watchedPaths) {
+            final Map<String, List<Store>> pathRegistration = watchedPaths.get(dirPath);
+            if (pathRegistration != null) {
+                final List<Store> pathStores = pathRegistration.get(fileName);
+                if (pathStores != null) {
+                    // Remove all references to this store.
+                    Iterator<Store> storeIterator = pathStores.iterator();
+                    while (storeIterator.hasNext()) {
+                        Store current = storeIterator.next();
+                        if (keyStore == current) {
+                            storeIterator.remove();
+                        }
+                    }
+                    if (pathStores.isEmpty()) {
+                        pathRegistration.remove(fileName);
                     }
                 }
-                if (pathStores.isEmpty()) {
-                    pathRegistration.remove(fileName);
-                } else {
-                    // Other stores are still expecting notifications.
-                    pathRegistration.put(fileName, Collections.unmodifiableList(pathStores));
-                }
-            }
 
-            // Copy of the watched paths so the updated collections can be added / entries removed as needed.
-            Map<Path, Map<String, List<Store>>> newWatchedPaths = new HashMap<Path, Map<String, List<Store>>>(watchedPaths);
-            if (pathRegistration.isEmpty()) {
-                // This was the last registration watching anything in the directory specified.
-                newWatchedPaths.remove(dirPath);
-                // Remove and cancel the WatchKey registration.
-                WatchKey key = registrations.remove(dirPath);
-                if (key != null) {
-                    key.cancel();
+                if (pathRegistration.isEmpty()) {
+                    // This was the last registration watching anything in the directory specified.
+                    watchedPaths.remove(dirPath);
+                    // Remove and cancel the WatchKey registration.
+                    WatchKey key = registrations.remove(dirPath);
+                    if (key != null) {
+                        key.cancel();
+                    }
+                    // This could also have been the last directory overall being monitored, if so the WatchService can be
+                    // closed and discarded.
+                    if (watchedPaths.isEmpty()) {
+                        watchService.close();
+                        watchService = null;
+                    }
                 }
-                // This could also have been the last directory overall being monitored, if so the WatchService can be closed and discarded.
-                if (newWatchedPaths.isEmpty()) {
-                    watchService.close();
-                    watchService = null;
-                }
-            } else {
-                // Still some files to monitor so just set the updated Map for the current path.
-                newWatchedPaths.put(dirPath, Collections.unmodifiableMap(pathRegistration));
             }
-            // Finally set back the replacement Map.
-            watchedPaths = Collections.unmodifiableMap(newWatchedPaths);
         }
-
     }
 
     interface Store {
@@ -207,36 +176,43 @@ class KeyStoreWatcher {
                 while ((watchService = KeyStoreWatcher.this.watchService) != null) {
                     WatchKey key = watchService.take();
                     Path watchedPath = (Path) key.watchable();
-                    Map<String, List<Store>> pathRegistration = watchedPaths.get(watchedPath);
-                    // watchedPaths is volatile, any further writes above will not affect the pathRegistration reference we just obtained.
 
-                    // We create a list of Store implementations to notify as a single file could trigger multiple events, e.g. on Windows
+                    // We create a list of Store implementations to notify as a single file could trigger multiple events, e.g.
+                    // on Windows
                     // a rename can be seen as a CREATE followed by a MODIFY.
                     Set<Store> toNotify = new HashSet<Store>();
-                    if (pathRegistration != null) {
-                        for (WatchEvent<?> event : key.pollEvents()) {
-                            if (StandardWatchEventKinds.ENTRY_CREATE.equals(event.kind())
-                                    || StandardWatchEventKinds.ENTRY_MODIFY.equals(event.kind())) {
-                                Path context = (Path) event.context();
-                                String name = context.getFileName().toString();
-                                List<Store> stores = pathRegistration.get(name);
-                                if (stores != null) {
-                                    for (Store current : stores) {
-                                        toNotify.add(current);
-                                    }
-                                }
 
-                            } else if (StandardWatchEventKinds.OVERFLOW.equals(event.kind())) {
-                                // No idea what happened so reload them all.
-                                for (List<Store> stores : pathRegistration.values()) {
-                                    for (Store current : stores) {
-                                        toNotify.add(current);
+                    synchronized (watchedPaths) {
+                        Map<String, List<Store>> pathRegistration = watchedPaths.get(watchedPath);
+                        // watchedPaths is volatile, any further writes above will not affect the pathRegistration reference we
+                        // just obtained.
+
+                        if (pathRegistration != null) {
+                            for (WatchEvent<?> event : key.pollEvents()) {
+                                if (StandardWatchEventKinds.ENTRY_CREATE.equals(event.kind())
+                                        || StandardWatchEventKinds.ENTRY_MODIFY.equals(event.kind())) {
+                                    Path context = (Path) event.context();
+                                    String name = context.getFileName().toString();
+                                    List<Store> stores = pathRegistration.get(name);
+                                    if (stores != null) {
+                                        for (Store current : stores) {
+                                            toNotify.add(current);
+                                        }
+                                    }
+
+                                } else if (StandardWatchEventKinds.OVERFLOW.equals(event.kind())) {
+                                    // No idea what happened so reload them all.
+                                    for (List<Store> stores : pathRegistration.values()) {
+                                        for (Store current : stores) {
+                                            toNotify.add(current);
+                                        }
                                     }
                                 }
                             }
                         }
+                        key.reset(); // Reset the Key so subsequent modifications can be picked up.
                     }
-                    key.reset(); // Reset the Key so subsequent modifications can be picked up.
+
                     for (Store current : toNotify) {
                         current.modified();
                     }
