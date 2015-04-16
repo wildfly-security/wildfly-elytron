281ae17b9324f6cb61ed0f0930a7ad641dc22f32
Farah Juma <fjuma@redhat.com>
2015-04-16 14:01:04 -0400

    [ELY-162] Add the ability to specify a prefix and/or delimiter when draining code points to a string

--- src/test/java/org/wildfly/security/util/HexTest.java
+++ src/test/java/org/wildfly/security/util/HexTest.java
@@ -96,6 +96,14 @@ public class HexTest {
         verifyBackwardIterationOfEncodedCharacters(ci, encoded);
     }
 
+    @Test
+    public void testEncodeWithFormatting() {
+        assertEquals("66:6F:6F:62:61:72", CodePointIterator.ofString("foobar").asLatin1().hexEncode(true).drainToString(':', 2));
+        assertEquals("0x66 0x6F 0x6F 0x62 0x61 0x72", CodePointIterator.ofString("foobar").asLatin1().hexEncode(true).drainToString("0x", ' ', 2));
+        assertEquals("666F6F6261\n72666F6F62\n6172666F6F\n626172",
+                CodePointIterator.ofString("foobarfoobarfoobar").asLatin1().hexEncode(true).drainToString('\n', 10));
+    }
+
     private void verifyBackwardIterationOfEncodedCharacters(CodePointIterator ci, String encoded) {
         int encodedSize = encoded.length();
         for (int i = encodedSize - 1; i >= 0; i--) {
