9de652bed9f6d0cc123bf193fe9cc91eae4504dc
David M. Lloyd <david.lloyd@redhat.com>
2015-06-30 12:45:31 -0500

    SASL factories fixes and improvements

--- src/main/java/org/wildfly/security/sasl/util/SaslFactories.java
+++ src/main/java/org/wildfly/security/sasl/util/SaslFactories.java
@@ -18,6 +18,7 @@
 
 package org.wildfly.security.sasl.util;
 
+import java.security.Security;
 import java.util.Map;
 
 import javax.security.auth.callback.CallbackHandler;
@@ -26,6 +27,8 @@ import javax.security.sasl.SaslClientFactory;
 import javax.security.sasl.SaslException;
 import javax.security.sasl.SaslServer;
 import javax.security.sasl.SaslServerFactory;
+
+import org.wildfly.common.Assert;
 
 /**
  * A utility class for discovering SASL client and server factories.
@@ -37,43 +40,11 @@ public final class SaslFactories {
     private SaslFactories() {
     }
 
-    private static final SecurityProviderSaslClientFactory providerSaslClientFactory = new SecurityProviderSaslClientFactory();
-    private static final SecurityProviderSaslServerFactory providerSaslServerFactory = new SecurityProviderSaslServerFactory();
-
-    /**
-     * Get a standard SASL client factory which uses extended callbacks and searches both installed providers as well as
-     * {@code ServiceLoader}-style factories.
-     *
-     * @param classLoader the class loader from which to search
-     * @return the SASL client factory
-     */
-    public static SaslClientFactory getStandardSaslClientFactory(ClassLoader classLoader) {
-        return new AuthenticationCompleteCallbackSaslClientFactory(new AggregateSaslClientFactory(
-            new ServiceLoaderSaslClientFactory(classLoader),
-            providerSaslClientFactory
-        ));
-    }
-
-    /**
-     * Get a standard SASL server factory which uses extended callbacks and searches both installed providers as well as
-     * {@code ServiceLoader}-style factories.
-     *
-     * @param classLoader the class loader from which to search
-     * @return the SASL server factory
-     */
-    public static SaslServerFactory getStandardSaslServerFactory(ClassLoader classLoader) {
-        return new AuthenticationCompleteCallbackSaslServerFactory(new AggregateSaslServerFactory(
-            new ServiceLoaderSaslServerFactory(classLoader),
-            providerSaslServerFactory
-        ));
-    }
-
+    private static final SecurityProviderSaslClientFactory providerSaslClientFactory = new SecurityProviderSaslClientFactory(Security::getProviders);
+    private static final SecurityProviderSaslServerFactory providerSaslServerFactory = new SecurityProviderSaslServerFactory(Security::getProviders);
     private static final String[] NO_STRINGS = new String[0];
 
-    /**
-     * A {@link SaslClientFactory} which does not provide any mechanisms.
-     */
-    public static final SaslClientFactory EMPTY_SASL_CLIENT_FACTORY = new SaslClientFactory() {
+    private static final SaslClientFactory EMPTY_SASL_CLIENT_FACTORY = new SaslClientFactory() {
         public SaslClient createSaslClient(final String[] mechanisms, final String authorizationId, final String protocol, final String serverName, final Map<String, ?> props, final CallbackHandler cbh) throws SaslException {
             return null;
         }
@@ -83,10 +54,7 @@ public final class SaslFactories {
         }
     };
 
-    /**
-     * A {@link SaslServerFactory} which does not provide any mechanisms.
-     */
-    public static final SaslServerFactory EMPTY_SASL_SERVER_FACTORY = new SaslServerFactory() {
+    private static final SaslServerFactory EMPTY_SASL_SERVER_FACTORY = new SaslServerFactory() {
         public SaslServer createSaslServer(final String mechanism, final String protocol, final String serverName, final Map<String, ?> props, final CallbackHandler cbh) throws SaslException {
             return null;
         }
@@ -95,4 +63,84 @@ public final class SaslFactories {
             return NO_STRINGS;
         }
     };
+
+    /**
+     * Get a SASL client factory which returns an Elytron-provided mechanism.
+     *
+     * @return the SASL client factory (not {@code null})
+     */
+    public static SaslClientFactory getElytronSaslClientFactory() {
+        return new ServiceLoaderSaslClientFactory(SaslFactories.class.getClassLoader());
+    }
+
+    /**
+     * Get a SASL server factory which returns an Elytron-provided mechanism.
+     *
+     * @return the SASL server factory (not {@code null})
+     */
+    public static SaslServerFactory getElytronSaslServerFactory() {
+        return new ServiceLoaderSaslServerFactory(SaslFactories.class.getClassLoader());
+    }
+
+    /**
+     * Get a SASL client factory which searches all the given class loaders in order for mechanisms.
+     *
+     * @param classLoaders the class loaders to search
+     * @return the SASL client factory (not {@code null})
+     */
+    public static SaslClientFactory getSearchSaslClientFactory(ClassLoader... classLoaders) {
+        Assert.checkNotNullParam("classLoaders", classLoaders);
+        SaslClientFactory[] factories = new SaslClientFactory[classLoaders.length];
+        for (int i = 0, classLoadersLength = classLoaders.length; i < classLoadersLength; i++) {
+            factories[i] = new ServiceLoaderSaslClientFactory(classLoaders[i]);
+        }
+        return new AggregateSaslClientFactory(factories);
+    }
+
+    /**
+     * Get a SASL server factory which searches all the given class loaders in order for mechanisms.
+     *
+     * @param classLoaders the class loaders to search
+     * @return the SASL server factory (not {@code null})
+     */
+    public static SaslServerFactory getSearchSaslServerFactory(ClassLoader... classLoaders) {
+        Assert.checkNotNullParam("classLoaders", classLoaders);
+        SaslServerFactory[] factories = new SaslServerFactory[classLoaders.length];
+        for (int i = 0, classLoadersLength = classLoaders.length; i < classLoadersLength; i++) {
+            factories[i] = new ServiceLoaderSaslServerFactory(classLoaders[i]);
+        }
+        return new AggregateSaslServerFactory(factories);
+    }
+
+    /**
+     * Get a SASL client factory which uses the currently installed security providers to locate mechanisms.
+     *
+     * @return the SASL client factory (not {@code null})
+     */
+    public static SecurityProviderSaslClientFactory getProviderSaslClientFactory() {
+        return providerSaslClientFactory;
+    }
+
+    /**
+     * Get a SASL server factory which uses the currently installed security providers to locate mechanisms.
+     *
+     * @return the SASL server factory (not {@code null})
+     */
+    public static SecurityProviderSaslServerFactory getProviderSaslServerFactory() {
+        return providerSaslServerFactory;
+    }
+
+    /**
+     * Get a {@link SaslClientFactory} which does not provide any mechanisms.
+     */
+    public static SaslClientFactory getEmptySaslClientFactory() {
+        return EMPTY_SASL_CLIENT_FACTORY;
+    }
+
+    /**
+     * Get a {@link SaslServerFactory} which does not provide any mechanisms.
+     */
+    public static SaslServerFactory getEmptySaslServerFactory() {
+        return EMPTY_SASL_SERVER_FACTORY;
+    }
 }
