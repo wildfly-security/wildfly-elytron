262ea13aa6c61466ea48a99cfb4cffece872e7d7
pedroigor <pigor.craveiro@gmail.com>
2015-07-23 12:19:22 -0300

    [ELY-223] - Enchancements to the filesystem realm.

---
+++ src/test/java/org/wildfly/security/auth/FileSystemSecurityRealmTest.java
@@ -0,0 +1,317 @@
+/*
+ * JBoss, Home of Professional Open Source
+ *
+ * Copyright 2015 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.wildfly.security.auth;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.wildfly.security.WildFlyElytronProvider;
+import org.wildfly.security.auth.provider.FileSystemSecurityRealm;
+import org.wildfly.security.auth.server.ModifiableRealmIdentity;
+import org.wildfly.security.authz.Attributes;
+import org.wildfly.security.authz.AuthorizationIdentity;
+import org.wildfly.security.authz.MapAttributes;
+import org.wildfly.security.password.Password;
+import org.wildfly.security.password.PasswordFactory;
+import org.wildfly.security.password.interfaces.BCryptPassword;
+import org.wildfly.security.password.interfaces.ClearPassword;
+import org.wildfly.security.password.interfaces.DigestPassword;
+import org.wildfly.security.password.interfaces.SaltedSimpleDigestPassword;
+import org.wildfly.security.password.interfaces.ScramDigestPassword;
+import org.wildfly.security.password.interfaces.SimpleDigestPassword;
+import org.wildfly.security.password.spec.ClearPasswordSpec;
+import org.wildfly.security.password.spec.DigestPasswordAlgorithmSpec;
+import org.wildfly.security.password.spec.EncryptablePasswordSpec;
+import org.wildfly.security.password.spec.IteratedSaltedPasswordAlgorithmSpec;
+import org.wildfly.security.password.spec.SaltedPasswordAlgorithmSpec;
+import org.wildfly.security.password.util.PasswordUtil;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.security.Provider;
+import java.security.Security;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.wildfly.security.password.interfaces.BCryptPassword.BCRYPT_SALT_SIZE;
+
+/**
+ * @author <a href="mailto:psilva@redhat.com">Pedro Igor</a>
+ */
+public class FileSystemSecurityRealmTest {
+
+    private static final Provider provider = new WildFlyElytronProvider();
+
+    @BeforeClass
+    public static void onBefore() throws Exception {
+        Security.addProvider(provider);
+    }
+
+    @AfterClass
+    public static void onAfter() throws Exception {
+        Security.removeProvider(provider.getName());
+    }
+
+    @Test
+    public void testCreateIdentityWithNoLevels() throws Exception {
+        FileSystemSecurityRealm securityRealm = new FileSystemSecurityRealm(getRootPath(), 0);
+        ModifiableRealmIdentity identity = securityRealm.createRealmIdentity("plainUser");
+
+        assertFalse(identity.exists());
+
+        identity.create();
+
+        assertTrue(identity.exists());
+    }
+
+    @Test
+    public void testCreateIdentityWithLevels() throws Exception {
+        FileSystemSecurityRealm securityRealm = new FileSystemSecurityRealm(getRootPath(), 3);
+        ModifiableRealmIdentity identity = securityRealm.createRealmIdentity("plainUser");
+
+        identity.create();
+
+        assertTrue(identity.exists());
+    }
+
+    @Test
+    public void testCreateAndLoadIdentity() throws Exception {
+        FileSystemSecurityRealm securityRealm = new FileSystemSecurityRealm(getRootPath(), 3);
+        ModifiableRealmIdentity newIdentity = securityRealm.createRealmIdentity("plainUser");
+
+        newIdentity.create();
+
+        securityRealm = new FileSystemSecurityRealm(getRootPath(false), 3);
+
+        ModifiableRealmIdentity existingIdentity = securityRealm.createRealmIdentity("plainUser");
+
+        assertTrue(existingIdentity.exists());
+    }
+
+    @Test
+    public void testCreateAndLoadAndDeleteIdentity() throws Exception {
+        FileSystemSecurityRealm securityRealm = new FileSystemSecurityRealm(getRootPath(), 3);
+        ModifiableRealmIdentity newIdentity = securityRealm.createRealmIdentity("plainUser");
+
+        newIdentity.create();
+
+        securityRealm = new FileSystemSecurityRealm(getRootPath(false), 3);
+
+        ModifiableRealmIdentity existingIdentity = securityRealm.createRealmIdentity("plainUser");
+
+        assertTrue(existingIdentity.exists());
+
+        existingIdentity.delete();
+
+        assertFalse(existingIdentity.exists());
+
+        securityRealm = new FileSystemSecurityRealm(getRootPath(false), 3);
+
+        existingIdentity = securityRealm.createRealmIdentity("plainUser");
+
+        assertFalse(existingIdentity.exists());
+    }
+
+    @Test
+    public void testCreateIdentityWithAttributes() throws Exception {
+        FileSystemSecurityRealm securityRealm = new FileSystemSecurityRealm(getRootPath(), 1);
+        ModifiableRealmIdentity newIdentity = securityRealm.createRealmIdentity("plainUser");
+
+        newIdentity.create();
+
+        MapAttributes newAttributes = new MapAttributes();
+
+        newAttributes.addFirst("name", "plainUser");
+        newAttributes.addAll("roles", Arrays.asList("Employee", "Manager", "Admin"));
+
+        newIdentity.setAttributes(newAttributes);
+
+        securityRealm = new FileSystemSecurityRealm(getRootPath(false), 1);
+
+        ModifiableRealmIdentity existingIdentity = securityRealm.createRealmIdentity("plainUser");
+        AuthorizationIdentity authorizationIdentity = existingIdentity.getAuthorizationIdentity();
+        Attributes existingAttributes = authorizationIdentity.getAttributes();
+
+        assertEquals(newAttributes.size(), existingAttributes.size());
+        assertTrue(newAttributes.get("name").containsAll(existingAttributes.get("name")));
+        assertTrue(newAttributes.get("roles").containsAll(existingAttributes.get("roles")));
+    }
+
+    @Test
+    public void testCreateIdentityWithClearPassword() throws Exception {
+        char[] actualPassword = "secretPassword".toCharArray();
+        PasswordFactory factory = PasswordFactory.getInstance(ClearPassword.ALGORITHM_CLEAR);
+        ClearPassword clearPassword = (ClearPassword) factory.generatePassword(new ClearPasswordSpec(actualPassword));
+
+        assertCreateIdentityWithPassword(actualPassword, clearPassword);
+    }
+
+    @Test
+    public void testCreateIdentityWithBcryptCredential() throws Exception {
+        PasswordFactory passwordFactory = PasswordFactory.getInstance(BCryptPassword.ALGORITHM_BCRYPT);
+        char[] actualPassword = "secretPassword".toCharArray();
+        BCryptPassword bCryptPassword = (BCryptPassword) passwordFactory.generatePassword(
+                new EncryptablePasswordSpec(actualPassword, new IteratedSaltedPasswordAlgorithmSpec(10, PasswordUtil.generateRandomSalt(BCRYPT_SALT_SIZE)))
+        );
+
+        assertCreateIdentityWithPassword(actualPassword, bCryptPassword);
+    }
+
+    @Test
+    public void testCreateIdentityWithScramCredential() throws Exception {
+        char[] actualPassword = "secretPassword".toCharArray();
+        byte[] salt = PasswordUtil.generateRandomSalt(BCRYPT_SALT_SIZE);
+        PasswordFactory factory = PasswordFactory.getInstance(ScramDigestPassword.ALGORITHM_SCRAM_SHA_256);
+        EncryptablePasswordSpec encSpec = new EncryptablePasswordSpec(actualPassword, new IteratedSaltedPasswordAlgorithmSpec(4096, salt));
+        ScramDigestPassword scramPassword = (ScramDigestPassword) factory.generatePassword(encSpec);
+
+        assertCreateIdentityWithPassword(actualPassword, scramPassword);
+    }
+
+    @Test
+    public void testCreateIdentityWithDigest() throws Exception {
+        char[] actualPassword = "secretPassword".toCharArray();
+        PasswordFactory factory = PasswordFactory.getInstance(DigestPassword.ALGORITHM_DIGEST_SHA_512);
+        DigestPasswordAlgorithmSpec dpas = new DigestPasswordAlgorithmSpec("jsmith", "elytron");
+        EncryptablePasswordSpec encryptableSpec = new EncryptablePasswordSpec(actualPassword, dpas);
+        DigestPassword digestPassword = (DigestPassword) factory.generatePassword(encryptableSpec);
+
+        assertCreateIdentityWithPassword(actualPassword, digestPassword);
+    }
+
+    @Test
+    public void testCreateIdentityWithSimpleDigest() throws Exception {
+        char[] actualPassword = "secretPassword".toCharArray();
+        EncryptablePasswordSpec eps = new EncryptablePasswordSpec(actualPassword, null);
+        PasswordFactory passwordFactory = PasswordFactory.getInstance(SimpleDigestPassword.ALGORITHM_SIMPLE_DIGEST_SHA_512);
+        SimpleDigestPassword tsdp = (SimpleDigestPassword) passwordFactory.generatePassword(eps);
+
+        assertCreateIdentityWithPassword(actualPassword, tsdp);
+    }
+
+    @Test
+    public void testCreateIdentityWithSimpleSaltedDigest() throws Exception {
+        char[] actualPassword = "secretPassword".toCharArray();
+        byte[] salt = PasswordUtil.generateRandomSalt(BCRYPT_SALT_SIZE);
+        SaltedPasswordAlgorithmSpec spac = new SaltedPasswordAlgorithmSpec(salt);
+        EncryptablePasswordSpec eps = new EncryptablePasswordSpec(actualPassword, spac);
+        PasswordFactory passwordFactory = PasswordFactory.getInstance(SaltedSimpleDigestPassword.ALGORITHM_PASSWORD_SALT_DIGEST_SHA_512);
+        SaltedSimpleDigestPassword tsdp = (SaltedSimpleDigestPassword) passwordFactory.generatePassword(eps);
+
+        assertCreateIdentityWithPassword(actualPassword, tsdp);
+    }
+
+    @Test
+    public void testCreateIdentityWithEverything() throws Exception {
+        FileSystemSecurityRealm securityRealm = new FileSystemSecurityRealm(getRootPath(), 1);
+        ModifiableRealmIdentity newIdentity = securityRealm.createRealmIdentity("plainUser");
+
+        newIdentity.create();
+
+        MapAttributes newAttributes = new MapAttributes();
+
+        newAttributes.addFirst("firstName", "John");
+        newAttributes.addFirst("lastName", "Smith");
+        newAttributes.addAll("roles", Arrays.asList("Employee", "Manager", "Admin"));
+
+        newIdentity.setAttributes(newAttributes);
+
+        ArrayList<Object> credentials = new ArrayList<>();
+
+        PasswordFactory passwordFactory = PasswordFactory.getInstance(BCryptPassword.ALGORITHM_BCRYPT);
+        BCryptPassword bCryptPassword = (BCryptPassword) passwordFactory.generatePassword(
+                new EncryptablePasswordSpec("secretPassword".toCharArray(), new IteratedSaltedPasswordAlgorithmSpec(10, PasswordUtil.generateRandomSalt(BCRYPT_SALT_SIZE)))
+        );
+
+        credentials.add(bCryptPassword);
+
+        newIdentity.setCredentials(credentials);
+
+        securityRealm = new FileSystemSecurityRealm(getRootPath(false), 1);
+
+        ModifiableRealmIdentity existingIdentity = securityRealm.createRealmIdentity("plainUser");
+
+        assertTrue(existingIdentity.exists());
+        assertTrue(existingIdentity.verifyCredential("secretPassword".toCharArray()));
+
+        AuthorizationIdentity authorizationIdentity = existingIdentity.getAuthorizationIdentity();
+        Attributes existingAttributes = authorizationIdentity.getAttributes();
+
+        assertEquals(newAttributes.size(), existingAttributes.size());
+        assertTrue(newAttributes.get("firstName").containsAll(existingAttributes.get("firstName")));
+        assertTrue(newAttributes.get("lastName").containsAll(existingAttributes.get("lastName")));
+        assertTrue(newAttributes.get("roles").containsAll(existingAttributes.get("roles")));
+    }
+
+    public void assertCreateIdentityWithPassword(char[] actualPassword, Password credential) throws Exception {
+        FileSystemSecurityRealm securityRealm = new FileSystemSecurityRealm(getRootPath(), 1);
+        ModifiableRealmIdentity newIdentity = securityRealm.createRealmIdentity("plainUser");
+
+        newIdentity.create();
+
+        ArrayList<Object> credentials = new ArrayList<>();
+
+        credentials.add(credential);
+
+        newIdentity.setCredentials(credentials);
+
+        securityRealm = new FileSystemSecurityRealm(getRootPath(false), 1);
+
+        ModifiableRealmIdentity existingIdentity = securityRealm.createRealmIdentity("plainUser");
+
+        assertTrue(existingIdentity.exists());
+        assertTrue(existingIdentity.verifyCredential(actualPassword));
+    }
+
+    private Path getRootPath(boolean deleteIfExists) throws Exception {
+        Path rootPath = Paths.get(getClass().getResource(File.separator).toURI())
+                .resolve("filesystem-realm");
+
+        if (rootPath.toFile().exists() && !deleteIfExists) {
+            return rootPath;
+        }
+
+        return Files.walkFileTree(Files.createDirectories(rootPath), new SimpleFileVisitor<Path>() {
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                Files.delete(file);
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                Files.delete(dir);
+                return FileVisitResult.CONTINUE;
+            }
+        });
+    }
+
+    private Path getRootPath() throws Exception {
+        return getRootPath(true);
+    }
+
+}
