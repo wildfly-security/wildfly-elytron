8cf91485fad7a13288ecccd3971669ade9e66217
Farah Juma <fjuma@redhat.com>
2015-09-11 19:46:37 -0400

    [ELY-259] Modify EntityTest to use real callback handlers

--- src/test/java/org/wildfly/security/sasl/test/SaslServerBuilder.java
+++ src/test/java/org/wildfly/security/sasl/test/SaslServerBuilder.java
@@ -27,6 +27,8 @@ import java.security.Permissions;
 import java.security.spec.KeySpec;
 import java.util.Map;
 
+import javax.net.ssl.X509KeyManager;
+import javax.net.ssl.X509TrustManager;
 import javax.security.sasl.SaslException;
 import javax.security.sasl.SaslServer;
 import javax.security.sasl.SaslServerFactory;
@@ -39,9 +41,12 @@ import org.wildfly.security.password.PasswordFactory;
 import org.wildfly.security.password.interfaces.ClearPassword;
 import org.wildfly.security.password.spec.ClearPasswordSpec;
 import org.wildfly.security.sasl.util.ChannelBindingSaslServerFactory;
+import org.wildfly.security.sasl.util.CredentialSaslServerFactory;
+import org.wildfly.security.sasl.util.KeyManagerCredentialSaslServerFactory;
 import org.wildfly.security.sasl.util.PropertiesSaslServerFactory;
 import org.wildfly.security.sasl.util.ProtocolSaslServerFactory;
 import org.wildfly.security.sasl.util.ServerNameSaslServerFactory;
+import org.wildfly.security.sasl.util.TrustManagerSaslServerFactory;
 
 /**
  * @author Kabir Khan
@@ -63,6 +68,10 @@ public class SaslServerBuilder {
     private Tuple<String, byte[]> bindingTypeAndData;
     private String protocol;
     private String serverName;
+    private X509TrustManager trustManager;
+    private X509KeyManager keyManager;
+    private Object credential;
+    private String algorithm;
     private boolean dontAssertBuiltServer;
 
     public SaslServerBuilder(Class<? extends SaslServerFactory> serverFactoryClass, String mechanismName) {
@@ -130,6 +139,22 @@ public class SaslServerBuilder {
         return this;
     }
 
+    public SaslServerBuilder setTrustManager(final X509TrustManager trustManager) {
+        this.trustManager = trustManager;
+        return this;
+    }
+
+    public SaslServerBuilder setKeyManager(final X509KeyManager keyManager) {
+        this.keyManager = keyManager;
+        return this;
+    }
+
+    public SaslServerBuilder setCredential(final Object credential, final String algorithmName) {
+        this.credential = credential;
+        this.algorithm = algorithmName;
+        return this;
+    }
+
     public SaslServerBuilder setDontAssertBuiltServer() {
         this.dontAssertBuiltServer = true;
         return this;
@@ -166,6 +191,15 @@ public class SaslServerBuilder {
         }
         if (serverName != null) {
             factory = new ServerNameSaslServerFactory(factory, serverName);
+        }
+        if (trustManager != null) {
+            factory = new TrustManagerSaslServerFactory(factory, trustManager);
+        }
+        if (keyManager != null) {
+            factory = new KeyManagerCredentialSaslServerFactory(factory, keyManager);
+        }
+        if (credential != null) {
+            factory = new CredentialSaslServerFactory(factory, credential, algorithm);
         }
         SaslServer server = domain.createNewAuthenticationContext().createSaslServer(factory, mechanismName);
         if (!dontAssertBuiltServer) {
