ad336670ca889eb163cc477ad3ab0fe899b5878a
Darran Lofthouse <darran.lofthouse@jboss.com>
2017-04-10 16:59:00 +0100

    [ELY-1070] Throw an exception if the server side credential can not be obtained.

--- src/main/java/org/wildfly/security/_private/ElytronMessages.java
+++ src/main/java/org/wildfly/security/_private/ElytronMessages.java
@@ -1428,6 +1428,9 @@ public interface ElytronMessages extends BasicLogger {
     @Message(id = 5172, value = "Unable to locate MechanismConfiguration for mechanism.")
     AuthenticationMechanismException unableToLocateMechanismConfiguration(@Cause Throwable cause);
 
+    @Message(id = 5173, value = "[%s] Unable to obtain server credential.")
+    AuthenticationMechanismException unableToObtainServerCredential(String mechName);
+
     /* http package */
 
     @Message(id = 6000, value = "Status code can not be set at this time.")
