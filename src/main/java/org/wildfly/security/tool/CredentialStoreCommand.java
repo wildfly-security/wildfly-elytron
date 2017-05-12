752b18efb472fbae3bd4b0d14b5c1540a6b4d6a9
ivassile <ivassile@redhat.com>
2017-05-12 10:43:09 -0400

    [ELY-1122] WildFly Elytron Tool, Vault Command --bulk-convert ignore --summary option.

--- src/main/java/org/wildfly/security/tool/CredentialStoreCommand.java
+++ src/main/java/org/wildfly/security/tool/CredentialStoreCommand.java
@@ -380,16 +380,18 @@ class CredentialStoreCommand extends Command {
         com.append("/subsystem=elytron/credential-store=cs:add(");
         com.append("relative-to=jboss.server.data.dir,");
         com.append("create=true,");
-        if (implProps.get("modifiable") != null) {
-            com.append("modifiable=" + implProps.get("modifiable") + ",");
-        }
-        if (implProps.get("location") != null) {
-            com.append("location=\"" + implProps.get("location") + "\",");
-        }
-        String props = formatPropertiesForCli(implProps);
-        if (!props.isEmpty()) {
-            com.append(props);
-            com.append(",");
+        if (implProps != null && !implProps.isEmpty()) {
+            if (implProps.get("modifiable") != null) {
+                com.append("modifiable=" + implProps.get("modifiable") + ",");
+            }
+            if (implProps.get("location") != null) {
+                com.append("location=\"" + implProps.get("location") + "\",");
+            }
+            String props = formatPropertiesForCli(implProps);
+            if (!props.isEmpty()) {
+                com.append(props);
+                com.append(",");
+            }
         }
         com.append("credential-reference={");
         com.append("clear-text=\"");
