0d99d5647d352c5a38f5dbb4eadd39a6ee259c68
Stuart Douglas <stuart.w.douglas@gmail.com>
2017-06-30 12:09:24 +1000

    ELY-1272 Add some null checks for useName, NamePrincipal and useAuthorizationName

--- src/main/java/org/wildfly/security/auth/client/AuthenticationConfiguration.java
+++ src/main/java/org/wildfly/security/auth/client/AuthenticationConfiguration.java
@@ -588,6 +588,7 @@ public final class AuthenticationConfiguration {
      * @return the new configuration
      */
     public AuthenticationConfiguration useName(String name) {
+        Assert.checkNotNullParam("name", name);
         return usePrincipal(new NamePrincipal(name));
     }
 
@@ -599,7 +600,7 @@ public final class AuthenticationConfiguration {
      * @return the new configuration
      */
     public AuthenticationConfiguration useAuthorizationName(String name) {
-        return useAuthorizationPrincipal(new NamePrincipal(name));
+        return useAuthorizationPrincipal(name == null ? null : new NamePrincipal(name));
     }
 
     /**
