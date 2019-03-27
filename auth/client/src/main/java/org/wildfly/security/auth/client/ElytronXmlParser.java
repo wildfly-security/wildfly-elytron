434a0a2eb6a5318b3f479c8e30b75f5e801efbd8
Farah Juma <fjuma@redhat.com>
2019-03-27 13:51:40 -0400

    [ELY-1777] Add version 1.4 of the Elytron client schema

--- auth/client/src/main/java/org/wildfly/security/auth/client/ElytronXmlParser.java
+++ auth/client/src/main/java/org/wildfly/security/auth/client/ElytronXmlParser.java
@@ -142,7 +142,8 @@ public final class ElytronXmlParser {
         VERSION_1_0_1("urn:elytron:1.0.1", VERSION_1_0),
         VERSION_1_1("urn:elytron:client:1.1", VERSION_1_0_1),
         VERSION_1_2("urn:elytron:client:1.2", VERSION_1_1),
-        VERSION_1_3("urn:elytron:client:1.3", VERSION_1_2);
+        VERSION_1_3("urn:elytron:client:1.3", VERSION_1_2),
+        VERSION_1_4("urn:elytron:client:1.4", VERSION_1_3);
 
         final String namespace;
 
