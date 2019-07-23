8571346efbaf91defec11a38983e4edf721c8602
Bhaskar Gupta <guptab3@gmail.com>
2019-07-23 16:10:23 +0530

    Update ScriptRoleDecoder.java

--- src/main/java/org/wildfly/security/script/engine/ScriptRoleDecoder.java
+++ src/main/java/org/wildfly/security/script/engine/ScriptRoleDecoder.java
@@ -17,7 +17,7 @@
  */
 
 package org.wildfly.security.script.engine;
-
+import org.wildfly.security.authz;
 import javax.script.Invocable;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
