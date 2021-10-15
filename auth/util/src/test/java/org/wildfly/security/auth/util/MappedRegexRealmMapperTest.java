7164d56f7edab5af4048d133a105da785c3a2c7b
Pedro Hos <pedro-hos@outlook.com>
2021-10-14 22:56:24 -0300

    [ELY-2171] Add test for MappedRegexRealmMapper
    [ELY-2171] Add test for MappedRegexRealmMapper
    
    [ELY-2171] Add test for MappedRegexRealmMapper
    
    [ELY-2171] Add test for MappedRegexRealmMapper #1601
    
    [ELY-2171] Add test for MappedRegexRealmMapper
    
    Add test for MappedRegexRealmMapper

---
+++ auth/util/src/test/java/org/wildfly/security/auth/util/MappedRegexRealmMapperTest.java
@@ -0,0 +1,63 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2021 Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.wildfly.security.auth.util;
+
+import java.security.Principal;
+import java.util.Collections;
+import java.util.Map;
+import java.util.regex.Pattern;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.wildfly.security.auth.principal.NamePrincipal;
+import org.wildfly.security.x500.X500PrincipalBuilder;
+
+/**
+ * @author <a href="mailto:pesilva@redhat.com">Pedro Silva</a>
+ */
+public class MappedRegexRealmMapperTest {
+
+    @Test
+    public void testReturnNullForNonNamePrincipalInstance() {
+        Map<String, String> realmNameMap = Collections.emptyMap();
+        Pattern pattern = Pattern.compile("([A-Z]-\\d{4})");
+        MappedRegexRealmMapper mappedRegexRealmMapper = new MappedRegexRealmMapper(pattern, realmNameMap);
+        Principal principal = new X500PrincipalBuilder().build();
+        Assert.assertNull(mappedRegexRealmMapper.getRealmMapping(principal, null));
+    }
+
+    @Test
+    public void testReturnNullCantFindValueInMap() {
+        Map<String, String> realmNameMap = Collections.singletonMap("A-0000", "new_realm");
+        Pattern pattern = Pattern.compile("([A-Z]-\\d{4})");
+        MappedRegexRealmMapper mappedRegexRealmMapper = new MappedRegexRealmMapper(pattern, realmNameMap);
+        Principal principal = new NamePrincipal("A-9999");
+        Assert.assertNull(mappedRegexRealmMapper.getRealmMapping(principal, null));
+    }
+
+    @Test
+    public void testReturnSpecificRealmMapping() {
+        String realmName = "new_realm";
+        Map<String, String> realmNameMap = Collections.singletonMap("A-9999", realmName);
+        Pattern pattern = Pattern.compile("([A-Z]-\\d{4})");
+        MappedRegexRealmMapper mappedRegexRealmMapper = new MappedRegexRealmMapper(pattern, realmNameMap);
+        Principal principal = new NamePrincipal("A-9999");
+        Assert.assertEquals(realmName, mappedRegexRealmMapper.getRealmMapping(principal, null));
+    }
+}
