5c68af26fd7c7c43986439f0f9cdb894d3876f29
BuildTools <lvydra@redhat.com>
2021-11-04 14:04:19 +0100

    [ELY-2169] Add test for MutableNameRewriter

---
+++ tests/base/src/test/java/org/wildfly/security/auth/server/ServerUtils.java
@@ -0,0 +1,59 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2021 Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.wildfly.security.auth.server;
+
+import org.wildfly.security.auth.principal.NamePrincipal;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+
+public class ServerUtils {
+
+    public static void addUser(ModifiableSecurityRealm realm, String userName) throws RealmUnavailableException {
+        ModifiableRealmIdentity realmIdentity = realm.getRealmIdentityForUpdate(new NamePrincipal(userName));
+        realmIdentity.create();
+        realmIdentity.dispose();
+    }
+
+    public static Path getRootPath(boolean deleteIfExists, Class<?> testClass) throws Exception {
+        Path rootPath = Paths.get(testClass.getResource(File.separator).toURI()).resolve("filesystem-realm");
+
+        if (rootPath.toFile().exists() && !deleteIfExists) {
+            return rootPath;
+        }
+
+        return Files.walkFileTree(Files.createDirectories(rootPath), new SimpleFileVisitor<Path>() {
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                Files.delete(file);
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult postVisitDirectory(Path dir, IOException exc) {
+                return FileVisitResult.CONTINUE;
+            }
+        });
+    }
+}
