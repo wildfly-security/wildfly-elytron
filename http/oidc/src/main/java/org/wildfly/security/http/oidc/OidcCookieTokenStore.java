357257ea94b581d18d484f08d32098d029ec238b
Christoph Böhme <christoph.boehme@dfg.de>
2022-07-19 15:03:02 +0200

    [ELY-2360] Accept cookies containing old delimiter
    Commit 3a654474cb52194f64512bd75a251cc0b72d8302 changed the delimiter
    used to separate tokens in cookies. This change renders existing cookies
    which still use the old delimiters invalid, though. To avoid this, a
    fallback for the old delimiter is added.

--- http/oidc/src/main/java/org/wildfly/security/http/oidc/OidcCookieTokenStore.java
+++ http/oidc/src/main/java/org/wildfly/security/http/oidc/OidcCookieTokenStore.java
@@ -37,6 +37,7 @@ public class OidcCookieTokenStore implements OidcTokenStore {
 
     private final OidcHttpFacade httpFacade;
     private static final String DELIM = "###";
+    private static final String LEGACY_DELIM = "___";
     private static final int EXPECTED_NUM_TOKENS = 3;
     private static final int ACCESS_TOKEN_INDEX = 0;
     private static final int ID_TOKEN_INDEX = 1;
@@ -205,6 +206,11 @@ public class OidcCookieTokenStore implements OidcTokenStore {
         }
         String cookieVal = cookie.getValue();
         String[] tokens = cookieVal.split(DELIM);
+        if (tokens.length != EXPECTED_NUM_TOKENS) {
+            // Cookies set by older versions of wildfly-elytron use a different token delimiter. Since clients may
+            // still send such cookies we fall back to the old delimiter to avoid discarding valid tokens:
+            tokens = cookieVal.split(LEGACY_DELIM);
+        }
         if (tokens.length != EXPECTED_NUM_TOKENS) {
             log.warnf("Invalid format of %s cookie. Count of tokens: %s, expected %s", OIDC_STATE_COOKIE, tokens.length, EXPECTED_NUM_TOKENS);
             log.debugf("Value of %s cookie is: %s", OIDC_STATE_COOKIE, cookieVal);
