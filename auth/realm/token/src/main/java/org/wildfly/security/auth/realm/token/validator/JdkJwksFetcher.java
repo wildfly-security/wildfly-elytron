dc1de9671115cf88d4b1efa344749c99166a2034
Raul Ojeda Robles <raw_ojeda99@hotmail.com>
2026-08-11 13:33:01 +0100

    [ELY-2924] Add JWKS HTTP transport layer. JwksFetcher interface and implementations: Introduce JwksFetcher(Functional Interface) and JwksException in the oidc/jwks module as the HTTP abstraction for fetching JWKS documents. Add JdkJwksFetcher(auth/realm/token) and ApacheHttpJwksFetcher(http/oidc) as the two concrete implementations. No consumers wired yet.
    Signed-off-by: Raul Ojeda Robles <raw_ojeda99@hotmail.com>

---
+++ auth/realm/token/src/main/java/org/wildfly/security/auth/realm/token/validator/JdkJwksFetcher.java
@@ -0,0 +1,77 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2026 Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.wildfly.security.auth.realm.token.validator;
+
+import org.wildfly.security.oidc.jwks.JwksException;
+import org.wildfly.security.oidc.jwks.JwksFetcher;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+
+/**
+ * A {@link JwksFetcher} that uses {@link HttpsURLConnection} to fetch JWKS documents.
+ *
+ * <p>Enforces HTTPS-only: throws {@link JwksException} if the URL scheme is not HTTPS.
+ * Used by {@link JwtValidator} in the token-realm module.
+ *
+ * @author <a href="mailto:rojeda@redhat.com">Raul Ojeda Robles</a>
+ */
+class JdkJwksFetcher implements JwksFetcher {
+
+    private final SSLContext sslContext;
+    private final HostnameVerifier hostnameVerifier;
+    private final int connectionTimeoutMs;
+    private final int readTimeoutMs;
+
+    JdkJwksFetcher(SSLContext sslContext, HostnameVerifier hostnameVerifier,
+                   int connectionTimeoutMs, int readTimeoutMs) {
+        this.sslContext = sslContext;
+        this.hostnameVerifier = hostnameVerifier;
+        this.connectionTimeoutMs = connectionTimeoutMs;
+        this.readTimeoutMs = readTimeoutMs;
+    }
+
+    @Override
+    public byte[] fetch(URL url) throws JwksException {
+        URLConnection connection;
+        try {
+            connection = url.openConnection();
+        } catch (IOException e) {
+            throw new JwksException("Failed to open connection to " + url, e);
+        }
+        if (!(connection instanceof HttpsURLConnection)) {
+            throw new JwksException("JWKS endpoint must use HTTPS: " + url);
+        }
+        HttpsURLConnection httpsConn = (HttpsURLConnection) connection;
+        httpsConn.setSSLSocketFactory(sslContext.getSocketFactory());
+        httpsConn.setHostnameVerifier(hostnameVerifier);
+        httpsConn.setConnectTimeout(connectionTimeoutMs);
+        httpsConn.setReadTimeout(readTimeoutMs);
+        try {
+            httpsConn.setRequestMethod("GET");
+            httpsConn.connect();
+            return httpsConn.getInputStream().readAllBytes();
+        } catch (IOException e) {
+            throw new JwksException("Failed to fetch JWKS from " + url, e);
+        }
+    }
+}
---
+++ oidc/jwks/src/main/java/org/wildfly/security/oidc/jwks/JwksFetcher.java
@@ -0,0 +1,39 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2026 Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.wildfly.security.oidc.jwks;
+
+import java.net.URL;
+
+/**
+ * HTTP transport abstraction for fetching a JWKS document from a remote endpoint.
+ *
+ * @author <a href="mailto:rojeda@redhat.com">Raul Ojeda Robles</a>
+ */
+@FunctionalInterface
+public interface JwksFetcher {
+
+    /**
+     * Fetch the JWKS document from the given URL.
+     *
+     * @param url the JWKS endpoint URL
+     * @return the raw response body as bytes (expected to be a JSON JWKS document)
+     * @throws JwksException if the fetch fails for any reason (I/O error, non-200 status, etc.)
+     */
+    byte[] fetch(URL url) throws JwksException;
+}
