1c568d577d7de4ebcfb703a80bf6d48b90187ef9
Ohifeme <ohifeme14@gmail.com>
2026-08-14 11:46:07 -0400

    ELY-3066 Add publicKeyUrl support to JwtValidator for remote public key retrieval

--- auth/realm/token/src/main/java/org/wildfly/security/auth/realm/token/_private/ElytronMessages.java
+++ auth/realm/token/src/main/java/org/wildfly/security/auth/realm/token/_private/ElytronMessages.java
@@ -102,5 +102,13 @@ public interface ElytronMessages extends BasicLogger {
     @LogMessage(level = WARN)
     @Message(id = 1182, value = "Allowed jku values haven't been configured for the JWT validator. Token validation will fail if the token contains a 'jku' header parameter.")
     void allowedJkuValuesNotConfigured();
+
+    @LogMessage(level = WARN)
+    @Message(id = 1183, value = "Unable to retrieve public key from remote URL \"%1$s\".")
+    void unableToFetchRemotePublicKey(String url);
+
+    @LogMessage(level = WARN)
+    @Message(id = 1184, value = "Not sending new request to remote public key URL \"%s\". Last request time was %d.")
+    void avoidingFetchRemotePublicKey(URL url, long timestamp);
 }
 
